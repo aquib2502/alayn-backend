23c3cbf067ec758e8a9d45e5835ee181d3359981
noumankhan20 <noumankhan8097@gmail.com>
2026-07-21 12:54:52 +0530

    added image filed

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,8 @@ app.use(cors({
 
 // Request Parsers
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Swagger UI Documentation
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
